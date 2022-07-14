a8bde2e12dc06f2adfe848d674e72f29902c3709
Brian <brian.lockerbie@gmail.com>
2022-07-13 22:36:10 -0700

    added first project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,21 +11,23 @@ const Work = () => {
                 <p className='py-6'>// Check out some of my recent work</p>
             </div>
 
-            <div style={{backgroundImage: `url(${workImg})`}} 
-            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div className='shadow-lg shadow-[#040c16] group container rouunded-md flex justify-center items-center mx-auto content-div'>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div 
+                  style={{backgroundImage: `url(${workImg})`}}
+                  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+                  >
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white -tracking-wider'>
-    Accessibility Application
+                          Accessibility Application
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
-                                <button>Demo</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="/">
-                                <button>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
 
                         </div>
