62632b0f491d788d182e0ea1b9cfc9206655037c
Brian <brian.lockerbie@gmail.com>
2022-07-13 17:25:30 -0700

    added home page and styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,18 @@ export const Home = () => {
   return (
     <div className='w-full h-screen bg-[#333333]'>
 
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+          <p className='text-purple-600'>Hi, my name is</p>
+          <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Brian Lockerbie</h1>
+          <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front End Developer.</h2>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a front-end developer specializing in building and designing exceptional digital experiences. 
+          I excell in languages like HTML, CSS & JavaScript. I excell in my favorite framework React.</p>
+          <div>
+            <button>View Work <HiArrowNarrowRight /></button>
+          </div>
+        </div>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
 
 
           {/* Social Media Icons */}
-          <div className='flex fixed-col top-[35%] left-0'>
+          <div className=' hidden lg:flex fixed-col top-[35%] left-0'>
             <ul>
               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hove:ml-[-10px] duration-300 bg-blue-600'>
                 <a 
